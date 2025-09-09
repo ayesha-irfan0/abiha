95ff8ee45d037999bead7636c19ae93907415ea8
ayesha irfan <wwwayesha0irfan@gmail.com>
2025-09-09 19:37:57 +0500

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
-import { Hero } from "./components/hero"
-import { Features } from "./components/features"
-import { Footer } from "./components/footer"
-import { Header } from "./components/header"
+import { Hero } from "@/components/hero"
+import { Features } from "@/components/features"
+import { Footer } from "@/components/footer"
+import { Header } from "@/components/header"
 
 export default function HomePage() {
   return (
